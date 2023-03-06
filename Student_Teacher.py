0c5410933fa73f979f70455036fcce88c83c15f3
HunterOBrien <obrienh2@middleton.school.nz>
2023-03-06 20:03:53 +1300

    Update Student_Teacher.py

--- Student_Teacher.py
+++ Student_Teacher.py
@@ -2,6 +2,7 @@ import easygui
 import math
 
 nameOfSchool = easygui.enterbox("Enter the name of your school", "School Name")
+teacherNum = easygui.integerbox("How many teachers are currently at your school?", "Teachers At School")
 maxClassSize = easygui.integerbox("What is the maximum number of students you want in each class, Must be a Number "
                                   "between 10 and 30")
 
@@ -10,4 +11,10 @@ totalNumChildren = easygui.integerbox(f"What is the total number of children cur
 
 
 numClasses = math.ceil(totalNumChildren / maxClassSize)
-print(numClasses)
+
+if teacherNum >= numClasses:
+    easygui.msgbox(f"At {nameOfSchool}You have enough teachers at your school, you only need {numClasses}")
+
+else:
+    easygui.msgbox(f"At {nameOfSchool} You do no have enough teachers at your school, you need {numClasses}")
+
